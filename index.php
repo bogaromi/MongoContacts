5563e1410da5e2a4acec75afe373eafae7fad0a1
Zoltán Bogáromi <zoltan.bogaromi@gmail.com>
2023-03-23 19:40:37 +0100

    Model, style

--- index.php
+++ index.php
@@ -1,3 +1,47 @@
 <?php
 require_once 'vendor/autoload.php';
+require_once 'system/model.php';
+require_once 'system/begin.php';
 
+error_reporting(E_ALL);
+ini_set("display_errors", "On");
+
+
+Model::Init();
+/*Model::InsertContact([
+    'firstname' => "Kovács",
+    'lastname' => "Katalin",
+    'email' => "katalin@mailhost.hu",
+    'phonenumber' => "+36 30 1476674",
+]);*/
+/*$items = Model::SelectContacts();
+foreach ($items as $itm)
+{
+ var_dump($itm ['firstname']);
+ var_dump($itm ['lastname']);
+ var_dump($itm['_id']->serialize());
+ 
+}*/
+/*
+$id= 'a:1:{s:3:"oid";s:24:"641c85493e270f66e9068138";}';
+
+$item = Model::GetContactByIdString($id);
+var_dump($item ['firstname']);
+var_dump($item ['lastname']);
+ * */
+
+ /*$id= 'a:1:{s:3:"oid";s:24:"641c85493e270f66e9068138";}';
+ $item = Model::GetContactByIdString($id);
+ $r = Model::UpdateContact($id, [
+     'lastname' => "Katinka",
+     'email' => "kovacskata@hotmail.com"
+ ]);
+var_dump($r);
+  * 
+  */
+
+/*
+$id= 'a:1:{s:3:"oid";s:24:"641c85493e270f66e9068138";}';
+$r = Model::DeleteContactByIdString($idString);
+ * 
+ */
---
+++ system/Model.php
@@ -0,0 +1,56 @@
+<?php
+
+class Model
+{
+ 
+ public static function Init()
+ {
+  
+$uri = "mongodb://bogaromi:Medve756@ac-xffgdfx-shard-00-00.b0p6cng.mongodb.net:27017,ac-xffgdfx-shard-00-01.b0p6cng.mongodb.net:27017,ac-xffgdfx-shard-00-02.b0p6cng.mongodb.net:27017/?ssl=true&replicaSet=atlas-14hunp-shard-0&authSource=admin&retryWrites=true&w=majority";
+$client = new MongoDB\Client($uri);
+self::$db = $client->db_real_contact;
+  
+ }
+ public static function InsertContact($contact)
+ {
+  $collection = self::$db->contacts;
+  $collection->insertOne($contact);
+ }
+ public static function SelectContacts()
+ {
+  $collection = self::$db->contacts;
+  
+  $sort = ['firstname' => 1];
+  $list = $collection->find([], ['sort' => $sort]);
+  
+  return $list->toArray();
+ }
+ public static function GetContactByIdString($idString)
+ {
+  
+  
+  $collection = self::$db->contacts;
+  
+  $filter = self::CreateFilterByIdString($idString);
+  $item = $collection->find(['id' => $objectId]);
+  return $item;
+ }
+ public static function UpdateContact($idString, $data)
+ {
+   $collection = self::$db->contacts;
+    $filter = self::CreateFilterByIdString($idString);
+   return $collection->updateOne($filter, ['$set' => $data]);
+ }
+ 
+ private static function CreateFilterByIdString($idString) 
+ {
+  $objectId = new MongoDB\BSON\ObjectId();
+  $objectId->unserialize($idString);
+  $filter = ['_id' => $objectId];
+  
+  return $filter;
+ 
+ }
+ 
+  private static $db;
+}
