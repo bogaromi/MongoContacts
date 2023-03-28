d24d27ce86525136d39bbde402b01ce45175ed94
Zoltán Bogáromi <zoltan.bogaromi@gmail.com>
2023-03-28 17:10:48 +0200

    update gitignore

--- system/View.php
+++ system/View.php
@@ -4,7 +4,7 @@
   public static function ShowContacts($item)
   {
    echo '<section class="p-4 bg-white shadow mb-4">';
-   echo '<table class="table">';
+   echo '<table class="table table-striped table-hover">';
    echo '<thead><tr>';
    echo '<th>Megnevezés</th><th>E-mail</th><th>Telefon</th>';
    echo '</tr></thead><tbody>';
@@ -12,9 +12,9 @@
    foreach ($item as $obj)
    {
     echo '<tr>';
-    echo '<td>'. $obj['firstname']. '' . $obj['lastname']. '<td>';
-    echo '<td>'. $obj['email']. '<td>';
-    echo '<td>'. $obj['phonenumber']. '<td>';
+    echo '<td>'. $obj['firstname']. ' ' . $obj['lastname']. '</td>';
+    echo '<td>'. $obj['email']. '</td>';
+    echo '<td>'. $obj['phonenumber']. '</td>';
     echo '</tr>';
    }
    
